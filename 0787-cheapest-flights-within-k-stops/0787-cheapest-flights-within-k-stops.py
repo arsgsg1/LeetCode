930c768310b9c7116d929331e8e4207450e9f3e8
arsgsg1 <arsgsg1@gmail.com>
2023-01-29 23:02:46 +0900

    Time: 204 ms (50.58%), Space: 15.8 MB (13.58%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,29 +1,24 @@
 import heapq as hq
 from collections import defaultdict
-INF = int(10_0000_0000)
-MAX_N = 200
-def solution(n, flights, src, des, k):
-    graph = defaultdict(list)
-    dist = [[INF] * (MAX_N) for _ in range(MAX_N)]
-    for flight in flights:
-        st, en, cost = flight
-        graph[st].append((en, cost))
-    q = [(0, src, 0)]
-    dist[src][0] = 0
-    while q:
-        total_dist, cur, stop = hq.heappop(q)
-        if dist[cur][stop] > total_dist: continue
-        if stop > n: continue
-        for nxt, cost in graph[cur]:
-            if total_dist + cost < dist[nxt][stop + 1] and stop <= k:
-                hq.heappush(q, (total_dist + cost, nxt, stop + 1))
-                dist[nxt][stop + 1] = total_dist + cost
-    answer = min(dist[des])
-    if answer == INF:
-        return -1
-    else:
-        return answer
 class Solution:
-    
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        return solution(n, flights, src, dst, k)
+    def findCheapestPrice(self, n: int, flights: list[list[int]], src: int, dst: int, k: int) -> int:
+        INF = 10_0000_0000
+        q = []
+        graph = defaultdict(list)
+
+        # make graph
+        for f in flights:
+            graph[f[0]].append((f[1], f[2]))
+        dist = [[INF] * (n + 2) for _ in range(n + 2)]
+        dist[src][0] = 0
+        hq.heappush(q, (0, src, 0))
+        while q:
+            cur_cost, cur, stop = hq.heappop(q)
+            if dist[cur][stop] < cur_cost: continue
+            if stop > k: continue
+            for nxt, nxt_cost in graph[cur]:
+                if dist[cur][stop] + nxt_cost < dist[nxt][stop + 1]:
+                    dist[nxt][stop + 1] = cur_cost + nxt_cost
+                    hq.heappush(q, (cur_cost + nxt_cost, nxt, stop + 1))
+        answer = min(dist[dst])
+        return -1 if answer == INF else answer
