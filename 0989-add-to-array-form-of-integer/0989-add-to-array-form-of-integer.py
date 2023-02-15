4b658871e456efcd28644edeab42b02231a267e2
arsgsg1 <arsgsg1@gmail.com>
2023-02-15 20:41:04 +0900

    Time: 6869 ms (5.02%), Space: 15.1 MB (36.53%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,10 +1,6 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
         total = k
-        ans = []
         for digit, item in enumerate(num[::-1]):
             total += pow(10, digit) * item
-        while total > 0:
-            ans.append(total % 10)
-            total //= 10
-        return ans[::-1]
+        return list(map(int, str(total)))
