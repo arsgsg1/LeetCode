100ce0ac446a58cce5aac26b1b7aea2738d6de34
arsgsg1 <arsgsg1@gmail.com>
2023-08-12 14:39:48 +0900

    Time: 50 ms (97.45%), Space: 16.5 MB (76.28%) - LeetHub

--- 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.py
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.py
@@ -1,39 +1,10 @@
-class LinkedList:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
 class Solution:
     def processQueries(self, queries: list[int], m: int) -> list[int]:
-        # 연결리스트를 만든다
-        root = LinkedList(val=1)
-        tail = root
         answer = []
-        for i in range(2, m + 1):
-            new = LinkedList(val=i)
-            new.left = tail
-            tail.right = new
-            tail = new
-
-        # 원소를 찾는다
+        P = [i for i in range(1, m + 1)]
         for q in queries:
-            if q == root.val:
-                answer.append(0)
-                continue
-            insert = LinkedList(val=q)
-            insert.right = root
-            root.left = insert
-            cur = root
-            cnt = 0
-            while cur.val != q:
-                cur = cur.right
-                cnt += 1
-            # 찾은 노드의 연결을 끊는다
-            prev, next = cur.left, cur.right
-            if prev is not None:
-                prev.right = next
-            if next is not None:
-                next.left = prev
-            root = insert
-            answer.append(cnt)
+            idx = P.index(q)
+            answer.append(idx)
+            num = P.pop(idx)
+            P = [num] + P
         return answer
