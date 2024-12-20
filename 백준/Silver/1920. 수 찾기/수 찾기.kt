c10174ea524acb06b9b9227d428b936c1c1336ce
arsgsg1 <arsgsg1@gmail.com>
2024-12-20 23:32:00 +0900

    [Silver IV] Title: 수 찾기, Time: 1856 ms, Memory: 104020 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.kt
+++ 백준/Silver/1920. 수 찾기/수 찾기.kt
@@ -1,33 +1,7 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
-var N: Int = 0
-var M: Int = 0
-fun lowerBound(arr: List<Int>, target: Int): Int {
-    var (st, en) = 0 to arr.size
-    while (st < en) {
-        val mid = (st + en) / 2
-        if (target <= arr[mid]) en = mid
-        else st = mid + 1
-    }
-    return st
-}
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
-    N = Integer.parseInt(readLine())
-    var ST = StringTokenizer(readLine())
-    val nums = mutableListOf<Int>()
-    val queries = mutableListOf<Int>()
-    repeat(N) {
-        nums.add(Integer.parseInt(ST.nextToken()))
-    }
-    nums.sort()
-    M = Integer.parseInt(readLine())
-    ST = StringTokenizer(readLine())
-    repeat(M) {
-        queries.add(Integer.parseInt(ST.nextToken()))
-    }
-    queries.forEach { item ->
-        val idx = lowerBound(nums, item)
-        println(if (idx >= nums.size || nums[idx] != item) 0 else 1)
-    }
+fun main() {
+    readln()
+    val set = readln().trim().split(" ").map(String::toInt).toSet()
+    readln()
+    val arr = readln().trim().split(" ").map(String::toInt).toIntArray()
+    arr.map { if (it in set) println(1) else println(0) }
 }
