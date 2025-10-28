c7475aedc488d46b713334a50c589296526e462e
arsgsg1 <arsgsg1@gmail.com>
2025-10-28 19:47:15 +0900

    [Silver IV] Title: 괄호, Time: 88 ms, Memory: 12556 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.kt
@@ -0,0 +1,31 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val N = readLine().toInt()
+    repeat(N) {
+        val str = readLine()
+        val stack = LinkedList<Char>()
+        str.forEach { ch ->
+            when (ch) {
+                '(' -> stack.push(ch)
+                ')' -> {
+                    if (stack.isNotEmpty() && stack.peek() == '(') {
+                        stack.pop()
+                    } else {
+                        stack.push(ch)
+                    }
+                }
+            }
+        }
+        
+        if (stack.isNotEmpty()) {
+            println("NO")
+        } else {
+            println("YES")
+        }
+    }
+}
+/**
+ * 1. 괄호 문자열은 쌍이 맞아야 함 -> 스택으로 쌍이 이루어지는지 확인
+ * 2. 문자열을 순회하면서 스택의 top이 괄호 쌍을 이룬다면 pop
+ */
